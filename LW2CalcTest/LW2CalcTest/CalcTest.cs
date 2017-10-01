223574cb12e1757c7aa7af0f34c457bcb8bfbf51
Андрей <andrewgolik29@gmail.com>
2017-10-01 12:59:42 +0300

    Add calc and sum test

---
+++ LW2CalcTest/LW2CalcTest/CalcTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+
+namespace LW2CalcTest
+{
+    [TestFixture]
+    public class CalcTest
+    {
+        #region Variables
+        private double _doubleTenNegative = -10.0;
+        private double _doubleMaxValue = double.MaxValue;
+        private double _doubleMinValue = double.MinValue;
+        private double _doubleTen = 10.0;
+        private double _result;
+        #endregion
+
+        #region TestSum
+        [Test]
+        public void TestSumTenAndTenNegative()
+        {
+            _result = Calculator.Plus(_doubleTenNegative, _doubleTen);
+            Assert.AreEqual(0.0, _result);
+        }
+        [Test]
+        public void TestSumMaxAndMinValue()
+        {
+            _result = Calculator.Plus(_doubleMaxValue, _doubleMinValue);
+            Assert.AreEqual(0.0, _result);
+        }
+        public void TestSumMaxAndTen()
+        {
+            _result = Calculator.Plus(_doubleMaxValue, _doubleTen);
+            Assert.AreEqual(double.PositiveInfinity, _result);
+        }
+        public void TestSumTenNegativeAndMinValue()
+        {
+            _result = Calculator.Plus(_doubleTenNegative, _doubleMinValue);
+            Assert.AreEqual(double.NegativeInfinity, _result);
+        }
+        [Test]
+        public void TestSumMaxAndMaxValue()
+        {
+            _result = Calculator.Plus(_doubleMaxValue, _doubleMaxValue);
+            Assert.AreEqual(double.PositiveInfinity, _result);
+        }
+        #endregion
+    }
+}
---
+++ LW2CalcTest/LW2CalcTest/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace LW2CalcTest
+{
+    public class Calculator
+    {
+        public static double Plus(double op1, double op2)
+        {
+            return op1 + op2;
+        }
+        public static double Minus(double op1, double op2)
+        {
+            return op1 - op2;
+        }
+        public static double Divide(double op1, double op2)
+        {
+            return op1 / op2;
+        }
+        public static double Multiply(double op1, double op2)
+        {
+            return op1 * op2;
+        }
+    }
+}
