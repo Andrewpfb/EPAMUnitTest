b2a7df44ec819b0224d192aa5d2e7e026a619ddb
Andrew <Andrewgolik29@gmail.com>
2017-10-01 13:52:42 +0300

    Add RunTest.bat

---
+++ LW2CalcTest/RunTest.bat
@@ -0,0 +1,8 @@
+nunit3-console D:\GitHubRepository\EPAMUnitTest\LW2CalcTest\LW2CalcTest\bin\Debug\LW2CalcTest.dll
+@echo off
+
+set /p end=Press ENTER to continue!
+
+if "%end%"=="" (goto cont) else (goto :eof)
+
+:cont
