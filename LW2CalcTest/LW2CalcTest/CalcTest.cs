1bcb460e8b05fc1b99bcaea76b8d16ae30c0ea1e
Андрей <andrewgolik29@gmail.com>
2017-10-01 13:17:33 +0300

    Add test minus

--- LW2CalcTest/LW2CalcTest/CalcTest.cs
+++ LW2CalcTest/LW2CalcTest/CalcTest.cs
@@ -7,14 +7,17 @@ namespace LW2CalcTest
     public class CalcTest
     {
         #region Variables
+
         private double _doubleTenNegative = -10.0;
         private double _doubleMaxValue = double.MaxValue;
         private double _doubleMinValue = double.MinValue;
         private double _doubleTen = 10.0;
         private double _result;
+
         #endregion
 
         #region TestSum
+
         [Test]
         public void TestSumTenAndTenNegative()
         {
@@ -27,15 +30,17 @@ namespace LW2CalcTest
             _result = Calculator.Plus(_doubleMaxValue, _doubleMinValue);
             Assert.AreEqual(0.0, _result);
         }
+        [Test]
         public void TestSumMaxAndTen()
         {
             _result = Calculator.Plus(_doubleMaxValue, _doubleTen);
-            Assert.AreEqual(double.PositiveInfinity, _result);
+            Assert.AreEqual(double.MaxValue, _result);
         }
+        [Test]
         public void TestSumTenNegativeAndMinValue()
         {
             _result = Calculator.Plus(_doubleTenNegative, _doubleMinValue);
-            Assert.AreEqual(double.NegativeInfinity, _result);
+            Assert.AreEqual(double.MinValue, _result);
         }
         [Test]
         public void TestSumMaxAndMaxValue()
@@ -43,6 +48,42 @@ namespace LW2CalcTest
             _result = Calculator.Plus(_doubleMaxValue, _doubleMaxValue);
             Assert.AreEqual(double.PositiveInfinity, _result);
         }
+
+        #endregion
+
+        #region TestMinus
+
+        [Test]
+        public void TestMinusMaxAndMinValue()
+        {
+            _result = Calculator.Minus(_doubleMaxValue, _doubleMinValue);
+            Assert.AreEqual(double.PositiveInfinity, _result);
+        }
+        [Test]
+        public void TestMinusMaxAndMaxValue()
+        {
+            _result = Calculator.Minus(_doubleMaxValue, _doubleMaxValue);
+            Assert.AreEqual(0.0, _result);
+        }
+        [Test]
+        public void TestMinusTenNegativeAndTenNegative()
+        {
+            _result = Calculator.Minus(_doubleTenNegative, _doubleTenNegative);
+            Assert.AreEqual(0.0, _result);
+        }
+        [Test]
+        public void TestMinusTenAndTenNegative()
+        {
+            _result = Calculator.Minus(_doubleTen, _doubleTenNegative);
+            Assert.AreEqual(20.0, _result);
+        }
+        [Test]
+        public void TestMinusTenNegativeAndTen()
+        {
+            _result = Calculator.Minus(_doubleTenNegative, _doubleTen);
+            Assert.AreEqual(-20.0, _result);
+        }
+
         #endregion
     }
 }
