eb82ef0b60a8ccb2900072f92476b08890e5c2db
Andrew.Golik <andrewgolik29@gmail.com>
2017-10-17 22:16:17 +0300

    Update lw4

--- SeleniumWebDriver_lw4/SeleniumWebDriver_lw4/UnitTest1.cs
+++ SeleniumWebDriver_lw4/SeleniumWebDriver_lw4/UnitTest1.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumWebDriver_lw4
 {
     [TestClass]
     public class UnitTest1
     {
+        FirefoxDriver firefoxDriver;
         [TestMethod]
         public void TestMethod1()
         {
+            firefoxDriver = new FirefoxDriver();
+            firefoxDriver.Navigate().GoToUrl("https://github.com/");
         }
     }
 }
