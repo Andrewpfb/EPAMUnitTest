e4609f10b25cacf829100df452e7f3dccc8a66b2
Andrew.Golik <andrewgolik29@gmail.com>
2017-10-17 22:40:45 +0300

    lw 4 complete

--- SeleniumWebDriver_lw4/SeleniumWebDriver_lw4/UnitTest1.cs
+++ SeleniumWebDriver_lw4/SeleniumWebDriver_lw4/UnitTest1.cs
@@ -13,8 +13,13 @@ namespace SeleniumWebDriver_lw4
         [TestMethod]
         public void TestMethod1()
         {
-            firefoxDriver = new FirefoxDriver();
+            
+        firefoxDriver = new FirefoxDriver();
             firefoxDriver.Navigate().GoToUrl("https://github.com/");
+            firefoxDriver.FindElementByXPath(".//a[text() = 'Sign in']").Click();
+            firefoxDriver.FindElementById("login_field").SendKeys("testautomationuser");
+            firefoxDriver.FindElementById("password").SendKeys("Time4Death!");
+
         }
     }
 }
